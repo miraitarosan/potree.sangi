5c4db522772db20d3fb096d82e2af3ea0c5ec901
sangij120151-admin <san-j22029@sangi.jp>
2025-11-28 10:10:04 +0900

    first commit

---
+++ nakabori/pointclouds/index/cloud.js
@@ -0,0 +1,29 @@
+{
+    "version": "1.7",
+    "octreeDir": "data",
+    "projection": "",
+    "points": 155086312,
+    "boundingBox": {
+        "lx": -10941.351,
+        "ly": -113520.217,
+        "lz": 9.182000000000003,
+        "ux": -10376.418899999995,
+        "uy": -112955.2849,
+        "uz": 574.1141000000055
+    },
+    "tightBoundingBox": {
+        "lx": -10941.351,
+        "ly": -113520.217,
+        "lz": 9.182000000000003,
+        "ux": -10394.389000000001,
+        "uy": -112955.285,
+        "uz": 68.019
+    },
+    "pointAttributes": [
+        "POSITION_CARTESIAN",
+        "COLOR_PACKED"
+    ],
+    "spacing": 4.892455577850342,
+    "scale": 0.001,
+    "hierarchyStepSize": 5
+}
