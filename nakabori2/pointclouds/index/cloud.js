c30f76437b2d3b08e4db7f72996861fe2f4fa603
sangij120151-admin <san-j22029@sangi.jp>
2026-01-13 16:26:35 +0900

    Add Potree converted files

---
+++ nakabori2/pointclouds/index/cloud.js
@@ -0,0 +1,29 @@
+{
+    "version": "1.7",
+    "octreeDir": "data",
+    "projection": "",
+    "points": 220623917,
+    "boundingBox": {
+        "lx": -10951.314,
+        "ly": -113520.217,
+        "lz": 10.306000000000001,
+        "ux": -10380.138000000008,
+        "uy": -112949.04100000001,
+        "uz": 581.4819999999922
+    },
+    "tightBoundingBox": {
+        "lx": -10951.314,
+        "ly": -113520.217,
+        "lz": 10.306000000000001,
+        "ux": -10394.389000000001,
+        "uy": -112949.04100000001,
+        "uz": 76.131
+    },
+    "pointAttributes": [
+        "POSITION_CARTESIAN",
+        "COLOR_PACKED"
+    ],
+    "spacing": 4.946529388427734,
+    "scale": 0.001,
+    "hierarchyStepSize": 5
+}
